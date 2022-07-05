3fa337a209b4d5f91539e5ca0bffd9647a6be8bb
Mahmoud R. Alnakeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2022-07-05 16:21:29 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,12 +5,12 @@ let {
   DB_NAME,
   DB_USER,
   DB_PASS,
-  DATABASE_URL,
+  DB_HOST,
   DB_PORT
 } = process.env
 let client = new pg.Pool({
   database: DB_NAME,
-  host: DATABASE_URL,
+  host: DB_HOST,
   password: DB_PASS,
   user: DB_USER,
   port: DB_PORT
