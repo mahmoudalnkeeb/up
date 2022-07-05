0c649ec63f766c2ea6d5ecdfef33954272bbd4d8
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-07-05 16:40:41 +0200

    database url connection

--- config/db.js
+++ config/db.js
@@ -1,19 +1,21 @@
 const pg = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
-let {
-  DB_NAME,
-  DB_USER,
-  DB_PASS,
-  DATABASE_URL,
-  DB_PORT
-} = process.env
-let client = new pg.Pool({
-  database: DB_NAME,
-  host: DATABASE_URL,
-  password: DB_PASS,
-  user: DB_USER,
-  port: DB_PORT
-})
+let databaseUrl = process.env.DATABASE_URL
+// let {
+//   DB_NAME,
+//   DB_USER,
+//   DB_PASS,
+//   DATABASE_URL,
+//   DB_PORT
+// } = process.env
+// let client = new pg.Pool({
+//   database: DB_NAME,
+//   host: DATABASE_URL,
+//   password: DB_PASS,
+//   user: DB_USER,
+//   port: DB_PORT
+// })
+let client = new pg.Pool(databaseUrl)
 
 module.exports = client
