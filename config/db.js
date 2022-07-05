5f032bec8987cdc996709aad6ef164bc0790de48
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-07-05 16:43:24 +0200

    .

--- config/db.js
+++ config/db.js
@@ -1,38 +1,8 @@
-const pg = require('pg')
-const dotenv = require('dotenv')
-dotenv.config()
-<<<<<<< HEAD
-let databaseUrl = process.env.DATABASE_URL
-// let {
-//   DB_NAME,
-//   DB_USER,
-//   DB_PASS,
-//   DATABASE_URL,
-//   DB_PORT
-// } = process.env
-// let client = new pg.Pool({
-//   database: DB_NAME,
-//   host: DATABASE_URL,
-//   password: DB_PASS,
-//   user: DB_USER,
-//   port: DB_PORT
-// })
-let client = new pg.Pool(databaseUrl)
-=======
-let {
-  DB_NAME,
-  DB_USER,
-  DB_PASS,
-  DB_HOST,
-  DB_PORT
-} = process.env
-let client = new pg.Pool({
-  database: DB_NAME,
-  host: DB_HOST,
-  password: DB_PASS,
-  user: DB_USER,
-  port: DB_PORT
-})
->>>>>>> 3fa337a209b4d5f91539e5ca0bffd9647a6be8bb
+const pg = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+let databaseUrl = process.env.DATABASE_URL;
 
-module.exports = client
+let client = new pg.Pool(databaseUrl);
+
+module.exports = client;
