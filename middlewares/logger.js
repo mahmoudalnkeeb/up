4ed4c8c7ae1a7020973ebfe2ed245eb5cd9dbe44
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-07-15 15:43:22 +0200

    headers

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -3,7 +3,7 @@ const logger = (req, res, next) => {
   console.log(req.method);
   console.log(req.path);
   console.log(req.url);
-  console.log(req.headers['Content-Type']);
+  console.log(req.headers);
   next();
 };
 
